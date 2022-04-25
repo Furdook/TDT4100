15ea2a59c39ce99f4feefcb90f3674e7d4538845
Tai <timonas@stud.ntnu.no>
2022-04-25 13:40:13 +0200

    go

--- src/test/java/project/modules/TicketTest.java
+++ src/test/java/project/modules/TicketTest.java
@@ -17,21 +17,21 @@ public class TicketTest {
         movie = new Movie("Test Film", 5);
         theatre = new Theatre("Test Theatre", 50);
         screening = new Screening(movie, 0, theatre);
-        ticket = new Tickets(movie, screening, 1, false, "John");
+        ticket = new Tickets(movie, screening, 1, true, "John");
     }
 
     @Test
     public void testConstructor() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Tickets(movie, screening, 4, false, "John");
+            new Tickets(movie, screening, 4, true, "John");
         });
 
         Assertions.assertThrows(NullPointerException.class, () -> {
-            new Tickets(movie, null, 2, false, "John");
+            new Tickets(movie, null, 2, true, "John");
         });
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Tickets(movie, screening, 200, false, "John");
+            new Tickets(movie, screening, 200, true, "John");
         });
     }
     
